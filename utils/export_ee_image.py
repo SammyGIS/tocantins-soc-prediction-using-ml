d3b559aaaa5afd47b8164f9c65176703b205d902
SammyGIS <adedoyinsamuel25@gmail.com>
2024-01-30 22:52:25 +0100

    add a new function

--- utils/export_ee_image.py
+++ utils/export_ee_image.py
@@ -1,12 +1,13 @@
 import os
+import geemap
 
-def export_ee_image_list(image_list, image_name,out_folder):
+def export_ee_image_list(image_list, image_name,out_folder,region,scale):
     for image in image_list:
         print("Exporting image_name..........................................")
-        out_path = os.path.join(out_folder, 'elevation_clip.tif')
+        out_path = os.path.join(out_folder, os.path.basename(image))
         print(out_path)
         if os.path.exists(out_path):
             print(f'{os.path.join(image_name)} already exists.')
         else:
-            geemap.ee_export_image(image, out_path, scale=90, region=feature_cls)
+            geemap.ee_export_image(image, out_path, scale, region)
             print(f'{os.path.join(image_name)} downloaded successfully to {out_path}.')
