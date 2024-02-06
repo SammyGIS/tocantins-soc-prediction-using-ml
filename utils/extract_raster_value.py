0a607449db040c3dd59511e8af654e3e01ea4eef
SammyGIS <adedoyinsamuel25@gmail.com>
2024-02-06 21:12:19 +0100

    add option to convert ee.image

--- utils/extract_raster_value.py
+++ utils/extract_raster_value.py
@@ -1,4 +1,5 @@
 import geopandas as  gpd
+import ee
 import os
 
 def  get_value_to_point(point_data:gpd.GeoDataFrame, raster_list:list) -> gpd.GeoDataFrame:
@@ -9,14 +10,12 @@ def  get_value_to_point(point_data:gpd.GeoDataFrame, raster_list:list) -> gpd.Ge
             x = point.geometry.x
             y = point.geometry.y
             
+            if type(raster_dataset) == 'ee.image.Image':
+                raster_dataset.toArray()
             # Get the row and column indices of the point in the rasters
             row, col = raster_dataset.index(x, y)
             
             # Extract the values of slope, elevation, and land use at the location of the point
             point_value =  raster_dataset.read(1)[row, col]
         
-            # Append the values to the point's attributes
-            point_data.loc[index,os.path.basename(point_data)] = point_value
-            
-        # read the dataset to view the addeded columns
-    return point_data
+    return  point_value
