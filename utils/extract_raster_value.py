dd892eaa08def54549d2c0202d79fae82fe476cc
SammyGIS <adedoyinsamuel25@gmail.com>
2024-02-06 21:13:42 +0100

    get sampling points

--- utils/extract_raster_value.py
+++ utils/extract_raster_value.py
@@ -12,10 +12,14 @@ def  get_value_to_point(point_data:gpd.GeoDataFrame, raster_list:list) -> gpd.Ge
             
             if type(raster_dataset) == 'ee.image.Image':
                 raster_dataset.toArray()
-            # Get the row and column indices of the point in the rasters
-            row, col = raster_dataset.index(x, y)
-            
-            # Extract the values of slope, elevation, and land use at the location of the point
-            point_value =  raster_dataset.read(1)[row, col]
-        
+                # Get the row and column indices of the point in the rasters
+                row, col = raster_dataset.index(x, y) 
+                # Extract the values of slope, elevation, and land use at the location of the point
+                point_value =  raster_dataset.read(1)[row, col]
+            else:
+                # Get the row and column indices of the point in the rasters
+                row, col = raster_dataset.index(x, y)      
+                # Extract the values of slope, elevation, and land use at the location of the point
+                point_value =  raster_dataset.read(1)[row, col]
+
     return  point_value
