665ab13655a8e4d7ad78a7d9e3bfc87e36da18c4
SammyGIS <adedoyinsamuel25@gmail.com>
2024-04-25 15:18:09 +0100

    Edit export image list function in utils

--- utils/.ipynb_checkpoints/export_ee_image-checkpoint.py
+++ utils/.ipynb_checkpoints/export_ee_image-checkpoint.py
@@ -1,15 +1,20 @@
 import os
 import geemap
 
-def export_ee_image_list(image_list:list,ouput_name_list:list|str,
-                         out_folder:str,region:str,scale:int):
-    for image in image_list:
-        for output_name in ouput_name_list:
-            print("Exporting image_name..........................................")
-            out_path = os.path.join(out_folder,output_name + '.tif')
-            print(out_path)
-            if os.path.exists(out_path):
-                print(f'{os.path.join(out_path)} already exists.')
-            else:
+def export_ee_image_list(image_list: list, output_name_list: list,
+                         out_folder: str, region: str, scale: int):
+    if len(image_list) != len(output_name_list):
+        raise ValueError("Length of image_list and output_name_list must be the same.")
+        
+    for image, output_name in zip(image_list, output_name_list):
+        print(f"Exporting {output_name}...")
+        out_path = os.path.join(out_folder, output_name + '.tif')
+        
+        if os.path.exists(out_path):
+            print(f'{out_path} already exists.')
+        else:
+            try:
                 geemap.ee_export_image(image, out_path, scale, region)
-                print(f'{os.path.basename(image)} downloaded successfully to {out_path}.')
+                print(f'{output_name} downloaded successfully to {out_path}.')
+            except Exception as e:
+                print(f'Error exporting {output_name}: {str(e)}')
