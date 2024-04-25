82b435aa8d6cf6da94782dbf767744a11f3971e1
SammyGIS <adedoyinsamuel25@gmail.com>
2024-04-25 15:10:38 +0100

    Edit export image list function in utils

--- utils/.ipynb_checkpoints/export_ee_image-checkpoint.py
+++ utils/.ipynb_checkpoints/export_ee_image-checkpoint.py
@@ -1,13 +1,15 @@
 import os
 import geemap
 
-def export_ee_image_list(image_list,out_folder,region,scale):
+def export_ee_image_list(image_list:list,ouput_name_list:list|str,
+                         out_folder:str,region:str,scale:int):
     for image in image_list:
-        print("Exporting image_name..........................................")
-        out_path = os.path.join(out_folder, os.path.basename(image))
-        print(out_path)
-        if os.path.exists(out_path):
-            print(f'{os.path.join(out_path)} already exists.')
-        else:
-            geemap.ee_export_image(image, out_path, scale, region)
-            print(f'{os.path.basename(image)} downloaded successfully to {out_path}.')
+        for output_name in ouput_name_list:
+            print("Exporting image_name..........................................")
+            out_path = os.path.join(out_folder,output_name + '.tif')
+            print(out_path)
+            if os.path.exists(out_path):
+                print(f'{os.path.join(out_path)} already exists.')
+            else:
+                geemap.ee_export_image(image, out_path, scale, region)
+                print(f'{os.path.basename(image)} downloaded successfully to {out_path}.')
