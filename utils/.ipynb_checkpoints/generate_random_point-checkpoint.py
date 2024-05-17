b74c939e3e41b644c5ed1f277dd8ea395a426945
SammyGIS <adedoyinsamuel25@gmail.com>
2024-05-17 18:37:53 +0100

    download raster

---
+++ utils/.ipynb_checkpoints/generate_random_point-checkpoint.py
@@ -0,0 +1,22 @@
+import random 
+from shapely import Point
+import geopandas as gpd
+
+def generate_random_points(boundary=None, num_points=None, crs=None):
+    # Generate the random points within the shapefile boundary
+    points = []
+    while len(points) < num_points:
+        # Generate a random point within the extent of the boundary
+        x = random.uniform(boundary.bounds.minx, boundary.bounds.maxx)
+        y = random.uniform(boundary.bounds.miny, boundary.bounds.maxy)
+        point = Point(x, y)
+        
+        # Check if the point is within the boundary
+        if point.within(boundary.geometry.values[0]):
+            points.append(point)
+
+    # Convert the list of points to a GeoDataFrame
+    if crs is None:
+        crs = boundary.crs
+    random_points_gdf = gpd.GeoDataFrame(geometry=points, crs=crs)
+    return random_points_gdf
--- utils/export_ee_image.py
+++ utils/export_ee_image.py
@@ -2,7 +2,7 @@ import os
 import geemap
 
 def export_ee_image_list(image_list: list, output_name_list: list,
-                         out_folder: str, scale: int,region: str):
+                         out_folder: str, scale: int,region):
 
     if len(image_list) != len(output_name_list):
         raise ValueError("Length of image_list and output_name_list must be the same.")
@@ -10,13 +10,13 @@ def export_ee_image_list(image_list: list, output_name_list: list,
     for image, output_name in zip(image_list, output_name_list):
         print(f"Exporting {output_name}...")
         out_path = os.path.join(out_folder, output_name + '.tif')
-        print(output_path)
+
         
         if os.path.exists(out_path):
             print(f'{out_path} already exists.')
         else:
             try:
-                geemap.ee_export_image(image, out_path, scale, region)
+                geemap.ee_export_image(image, out_path, scale)
                 print(f'{output_name} downloaded successfully to {out_path}.')
             except Exception as e:
                 print(f'Error exporting {output_name}: {str(e)}')
