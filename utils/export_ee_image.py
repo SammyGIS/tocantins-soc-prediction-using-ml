0f97bc065b44d65a6e1f4325fe6ce25007c89c24
SammyGIS <adedoyinsamuel25@gmail.com>
2024-05-17 16:16:58 +0100

    adjust export

--- utils/export_ee_image.py
+++ utils/export_ee_image.py
@@ -2,7 +2,7 @@ import os
 import geemap
 
 def export_ee_image_list(image_list: list, output_name_list: list,
-                         out_folder: str, region: str, scale: int):
+                         out_folder: str, scale: int,region: str):
 
     if len(image_list) != len(output_name_list):
         raise ValueError("Length of image_list and output_name_list must be the same.")
@@ -10,6 +10,7 @@ def export_ee_image_list(image_list: list, output_name_list: list,
     for image, output_name in zip(image_list, output_name_list):
         print(f"Exporting {output_name}...")
         out_path = os.path.join(out_folder, output_name + '.tif')
+        print(output_path)
         
         if os.path.exists(out_path):
             print(f'{out_path} already exists.')
