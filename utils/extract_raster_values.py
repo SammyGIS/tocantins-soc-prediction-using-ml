0d04941d364f87e076fde98582d63e965763c8f9
SammyGIS <adedoyinsamuel25@gmail.com>
2024-05-17 19:51:18 +0100

    addpoint and extract raster values to the point then retun the point as GDF

--- utils/extract_raster_values.py
+++ utils/extract_raster_values.py
@@ -1,25 +1,27 @@
-import geopandas as  gpd
+import geopandas as gpd
 import ee
-import os
 
-def  get_value_to_point(point_data:gpd.GeoDataFrame, raster_list:list) -> gpd.GeoDataFrame:
-    # Get the values of slope, elevation, and land use at the location of the random points
+def get_value_to_point(point_data: gpd.GeoDataFrame, raster_list: list) -> gpd.GeoDataFrame:
+    # Iterate over each raster dataset
     for raster_dataset in raster_list:
-        for _, point in point_data.iterrows():
-            # Get the coordinates of the point
-            x = point.geometry.x
-            y = point.geometry.y
-            
-            if type(raster_dataset) == 'ee.image.Image':
-                raster_dataset.toArray()
-                # Get the row and column indices of the point in the rasters
-                row, col = raster_dataset.index(x, y) 
-                # Extract the values of slope, elevation, and land use at the location of the point
-                point_value =  raster_dataset.read(1)[row, col]
-            else:
-                # Get the row and column indices of the point in the rasters
-                row, col = raster_dataset.index(x, y)      
-                # Extract the values of slope, elevation, and land use at the location of the point
-                point_value =  raster_dataset.read(1)[row, col]
+        # Check if the raster dataset is an Earth Engine image
+        if isinstance(raster_dataset, ee.image.Image):
+            # Get the pixel values for each point using Earth Engine
+            values = raster_dataset.reduceRegions(collection=point_data, reducer=ee.Reducer.first(), scale=30)
+            # Merge the extracted values with the point data
+            point_data = point_data.merge(values, left_index=True, right_index=True)
+        else:
+            # Get the pixel values for each point using Rasterio
+            raster_values = []
+            for _, point in point_data.iterrows():
+                x = point.geometry.x
+                y = point.geometry.y
+                row, col = raster_dataset.index(x, y)
+                point_value = raster_dataset.read(1)[row, col]
+                # Add the raster value to the list
+                raster_values.append(point_value)
+            # Add the raster values as a new column to the GeoDataFrame
+            point_data[raster_dataset.name] = raster_values
+    
+    return point_data
 
-            return  point_value
